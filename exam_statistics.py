010518e9970c193594441ef3426ebfb05ddbff6e
My VO <kathy.nguyen198@gmail.com>
2024-02-27 18:37:21 +0100

    The Average

--- exam_statistics.py
+++ exam_statistics.py
@@ -1,5 +1,6 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
+#Print those grades
 def print_grades(grades_input):
   for grade in grades_input:
     print(grade)
@@ -10,4 +11,11 @@ print_grades(grades)
 def grades_sum(scores):
   return sum(scores)
 
-print(grades_sum(grades))
+print("The sum of scores : ", grades_sum(grades))
+
+# Computing the Average
+def grades_average(grades_input):
+  average = grades_sum(grades_input) / float(len(grades_input))
+  return average
+
+print("The Average : ", grades_average(grades))
